faa96feae23d13b4047d4beadf5de5c177c11e66
henrik <henrik.buller@omegapoint.se>
2024-12-12 19:27:14 +0100

    init cdk

---
+++ test/op-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OpCdk from '../lib/op-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/op-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OpCdk.OpCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
